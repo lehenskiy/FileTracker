b6f648f354ab1051cb4ac73490598e6c0e3adc2b
Alexey Sheetov <leshakykys@mail.ru>
2023-04-26 23:50:18 +0700

    Main refactoring

--- main.cpp
+++ main.cpp
@@ -8,43 +8,40 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    FileStatesTracker tracker = new FileStatesTracker();
-    FileStatesConsoleNotifier *notifier = FileStatesConsoleNotifier::getInstance();
+    FileStatesTracker* tracker = FileStatesTracker::getInstance();
     FilesCollection filesCollection;
+    if (tracker == nullptr) {
+        QCoreApplication::exit();
+    }
 
-    QObject::connect(&tracker,
+    QObject::connect(tracker,
                      &FileStatesTracker::fileExists,
-                     notifier,
                      &FileStatesConsoleNotifier::fileExistsNotify);
-    QObject::connect(&tracker,
+    QObject::connect(tracker,
                      &FileStatesTracker::fileNotExists,
-                     notifier,
                      &FileStatesConsoleNotifier::fileNotExistsNotify);
-    QObject::connect(&tracker,
+    QObject::connect(tracker,
                      &FileStatesTracker::fileModified,
-                     notifier,
                      &FileStatesConsoleNotifier::fileModifiedNotify);
-    QObject::connect(&tracker,
+    QObject::connect(tracker,
                      &FileStatesTracker::trackingEnded,
-                     notifier,
                      &FileStatesConsoleNotifier::trackingEndedNotify);
 
     QTimer timer;
     timer.setInterval(5000);
-    QObject::connect(&timer, &QTimer::timeout, &tracker, [&tracker, filesCollection] {
+    QObject::connect(&timer, &QTimer::timeout, tracker, [tracker, filesCollection] {
         filesCollection.addFromFile(R"(../filesList.txt)");
         filesCollection.add(R"(../filesList.txt)");
         filesCollection.remove(R"(C:\Example\Path\To\file.txt)");
         QList<QString> fileListToTrack = filesCollection.getCollection();
 
-        tracker.trackFromList(fileListToTrack);
+        tracker->trackFromList(fileListToTrack);
     });
     timer.start();
 
     int result = QCoreApplication::exec();
 
-    delete &tracker;
-    delete notifier;
+    delete tracker;
     delete &filesCollection;
 
     return result;
