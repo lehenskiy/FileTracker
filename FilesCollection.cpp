35678a09253cd84a0d304ee447d509ac8e807273
Alexey Sheetov <leshakykys@mail.ru>
2023-04-26 23:47:08 +0700

    File does not exist handling

--- FilesCollection.cpp
+++ FilesCollection.cpp
@@ -1,6 +1,7 @@
 #include "FilesCollection.h"
 #include <QFile>
 #include <QTextStream>
+#include <stdexcept>
 
 void FilesCollection::add(QString pathfile) const
 {
@@ -19,6 +20,10 @@ void FilesCollection::remove(QString pathfile) const
 void FilesCollection::addFromFile(QString pathfile) const
 {
     QFile sourceFile(pathfile);
+    if (!sourceFile.exists()) {
+        throw std::runtime_error("File does not exist");
+    }
+
     sourceFile.open(QIODevice::ReadOnly);
     QTextStream sourceStream(&sourceFile);
 
